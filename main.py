3e371999218a6e5525a3d831ef239c2fffb148be
Qile Wang <wangqile123@gmail.com>
2016-01-23 02:53:31 -0500

    added requirements

--- main.py
+++ main.py
@@ -2,19 +2,22 @@
 
 # Import the Flask Framework
 from flask import Flask, request
+import twilio.twiml
 app = Flask(__name__)
 # Note: We don't need to call run() since our application is embedded within
 # the App Engine WSGI application server.
+ 
+@app.route("/", methods=['GET', 'POST'])
+def hello_monkey():
+    """Respond to incoming calls with a simple text message."""
+ 
+    resp = twilio.twiml.Response()
+    resp.message("Hello, Mobile Monkey")
+    return str(resp)
 
-
-@app.route('/', methods=['GET', 'POST'])
-def texty():
-    """Return a friendly HTTP greeting."""
-    from_number = request.values.get('From', None)
-    from_body = request.values.get('Body', None)
-    return 'Hello World!'
-
-
+if __name__ == "__main__":
+    app.run(debug=True)
+    
 @app.errorhandler(404)
 def page_not_found(e):
     """Return a custom 404 error."""
