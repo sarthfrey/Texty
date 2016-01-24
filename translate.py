51e1bede132b63fdd46b4d4a95502a9f0bd23640
sarthfrey <sarth.frey@gmail.com>
2016-01-23 22:18:19 -0500

    Merge remote-tracking branch 'origin/master'

--- translate.py
+++ translate.py
@@ -5,15 +5,16 @@ Created on Sat Jan 23 19:14:26 2016
 @author: prcobol
 """
 
-import requests
+import urllib
 import json
 
 def buildURL(key,source, target,text):
     return "https://www.googleapis.com/language/translate/v2?q=" + text + "&source=" + source + "&target=" + target + "&key=" + key
 
 def getJSON(key,source, target,text):
-    url = buildURL(key,source, target,text)
-    data = requests.get(url).json()
+    url = buildURL(key,source,target,text)
+    response = urllib.urlopen(url)
+    data = json.load(response)
     return data
     
 def getTranslation(source,target,text):
@@ -21,3 +22,6 @@ def getTranslation(source,target,text):
     translation = data['data']['translations'][0]['translatedText']
     return translation
 
+
+#print getTranslation("fr","en","je suis un bon person")
+    
